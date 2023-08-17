5fbbd89691880b9e56f46a036fa7ee4ee3feafca
chetanmalviya513 <119923520+chetanmalviya513@users.noreply.github.com>
2023-08-17 14:53:43 +0530

    Add files via upload

--- Tringer_Quest.py
+++ Tringer_Quest.py
@@ -1,18 +1,2 @@
-"""for i in range(1,int(input())):
-	print(i*11)"""
-
-"""for i in range(1,int(input())):	
-	print(pow(i))*11"""
-
-"""for i in range(1,int(input())):
-	print(pow(i*11))
-"""
-
-for i in range(1,int(input())):
-	print(pow(10,1))*11
-
-
-"""for i in range:
-	i= int(i)
-	x = pow(1*i,3)
-	print(x)"""
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(i*(10**i-1)//9)

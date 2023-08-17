13534e4ab744880387c99a5ffdd823e2782e439b
chetanmalviya513 <119923520+chetanmalviya513@users.noreply.github.com>
2023-08-17 14:50:21 +0530

    Add files via upload

---
+++ Tringer_Quest.py
@@ -0,0 +1,18 @@
+"""for i in range(1,int(input())):
+	print(i*11)"""
+
+"""for i in range(1,int(input())):	
+	print(pow(i))*11"""
+
+"""for i in range(1,int(input())):
+	print(pow(i*11))
+"""
+
+for i in range(1,int(input())):
+	print(pow(10,1))*11
+
+
+"""for i in range:
+	i= int(i)
+	x = pow(1*i,3)
+	print(x)"""
